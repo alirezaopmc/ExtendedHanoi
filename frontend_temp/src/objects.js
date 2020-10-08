612b6f864e668a4bb8117744992643fc2bd7a05d
Alireza <sleepingjempa@outlook.com>
2020-10-08 23:56:29 +0330

    wtf dude

--- frontend_temp/src/objects.js
+++ frontend_temp/src/objects.js
@@ -1,13 +1,14 @@
 class Disk {
     constructor(radius) {
+        radius = scaled(radius)
         this.x = 0
         this.y = 0
         this.width = radius
         this.height = radius * .1
-    }
 
-    render() {
-        rect(this.x, this.y, this.width, this.height)
+        this.R = random(255)
+        this.G = random(80, 220)
+        this.B = random(220, 250)
     }
 }
 
@@ -23,27 +24,67 @@ class Bar {
         disks.forEach(radius => {
             this.disks.push(new Disk(radius))
         })
+    }
 
+    update() {
+        this.disks.sort()
         let diff = 0
-        for (let disk of this.disks) {
+        this.disks.forEach(disk => {
             let x = this.x
-            let y = 450 - this.y - disk.height / 2 - diff
+            let y = 650 - this.y - disk.height / 2 - diff
             disk.x = x
             disk.y = y
             diff += disk.height
-        }
+        })
     }
 
     push(radius) {
         let top = this.disks.slice(-1)[0]
         let disk = new Disk(radius)
-        disk.x = top.x
-        disk.y = top.y - top.height / 2 - disk.height / 2
         this.disks.push(disk)
     }
 
-    render() {
-        line(this.x, this.y, this.x, 450 - this.y)
-        this.disks.forEach(disk => disk.render())
+    pop() {
+        this.disks.pop()
     }
+
+    render() {
+        line(this.x, this.y, this.x, 650 - this.y)
+        this.update()
+        this.disks.forEach(disk => {
+            rect(disk.x, disk.y, disk.width, disk.height)
+        })
+    }
+}
+
+class Section {
+    constructor(x, y, width, height) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+        this.color = 255;
+    }
+
+    swapColor(x, y) {
+        if (this.collision(x, y)) {
+            this.color = 200
+        } else {
+            this.color = 255
+        }
+    }
+
+    render(x, y) {
+        push()
+        this.swapColor(x, y)
+        fill(this.color)
+        rect(this.x, this.y, this.width, this.height)
+        pop()
+    }
+
+    collision(x, y) {
+        return (this.x - this.width/2 < x && x < this.x + this.width/2
+            && this.y - this.height/2 < y && y < this.y + this.height/2) 
+    }
+
 }
--- frontend_temp/src/view.js
+++ frontend_temp/src/view.js
@@ -1,10 +1,40 @@
 let bars = []
+let sections = []
+let scale = 1
+
+let n = 0
+
+// Animation Start
+let animation
+function animate() {
+
+}
+
+function move(f, t) {
+
+}
+// Animation End
+
+function scaled (x) {
+    return x * scale;
+}
+
+function mousePressed() {
+    // sections.forEach(section => {
+    //     section.collision(mouseX, mouseY)
+    // })
+}
 
 function setup() {
-    createCanvas(600, 400)
-    bars.push(new Bar(100, 70))
-    bars.push(new Bar(300, 70))
-    bars.push(new Bar(500, 70))
+    createCanvas(900, 600)
+
+    sections.push(new Section(150, 300, 280, 580))
+    sections.push(new Section(450, 300, 280, 580))
+    sections.push(new Section(750, 300, 280, 580))
+
+    bars.push(new Bar(150, 70))
+    bars.push(new Bar(450, 70))
+    bars.push(new Bar(750, 70))
 
     bars[0].init([180, 170, 160, 150])
     bars[2].init([180, 140])
@@ -15,6 +45,8 @@ function setup() {
 
 function draw() {
     background(200)
+    sections.forEach(section => section.render(mouseX, mouseY))
     bars.forEach(bar => bar.render())
+
     
 }
