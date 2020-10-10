dc34a38b4012da4877f5357e5013ff7019316395
shayan balaee <shayanfall79@gmail.com>
2020-10-10 15:29:56 +0330

    setup electron

---
+++ frontend_temp/index.js
@@ -0,0 +1,22 @@
+const electron = require('electron');
+const { ipcMain } = require('electron/main');
+const path = require('path');
+
+const { app, BrowserWindow, Menu } = electron;
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 1300,
+    height: 850,
+    resizable: false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.removeMenu();
+
+  mainWindow.on('closed', () => app.quit());
+});
