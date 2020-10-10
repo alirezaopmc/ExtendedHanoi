b6becb1cbabde8c747e83f9db2dcf97b1e75b8a5
shayan balaee <shayanfall79@gmail.com>
2020-10-10 18:21:38 +0330

    bruh

--- backend/core/caller.js
+++ backend/core/caller.js
@@ -1,9 +1,9 @@
 const { execSync } = require('child_process');
 
 const solve = (hanoi) => {
-  let command = `/mnt/d/exhanoi.py '${hanoi}'`;
+  let command = `python /mnt/d/exhanoi.py '${hanoi}'`;
 
-  const result = '12,02,21,02'; //execSync(command).toString().replace('\n', '');
+  const result = '12,02,21,02'; //execSync(command).toString().replace('\n', ''); //'12,02,21,02';
   return result;
 };
 
