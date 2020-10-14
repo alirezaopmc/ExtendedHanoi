8b696ed3b385e126489458d3fef79c198698d1d6
Alireza <sleepingjempa@outlook.com>
2020-10-14 16:27:39 +0330

    fps stuff

--- frontend/src/view.js
+++ frontend/src/view.js
@@ -1,26 +1,30 @@
 let bars = [];
 let sections = [];
 let scale = 10;
+let fr = 30;
 
-let indexOfSpeed = 2
-let speeds = [1, 2, 5, 10]
-let speed = speeds[indexOfSpeed]
+let debug_items = [];
+let debug_functions = [];
 
-function changeSpeed(type) {
-  if (type == 'increase') {
-    if (indexOfSpeed + 1 < speeds.length) {
-      indexOfSpeed += 1
-    }
-  }
+// let indexOfSpeed = 2
+// let speeds = [1, 2, 5, 10]
+let speed = 1
 
-  if (type == 'decrease') {
-    if (indexOfSpeed - 1 >= 0) {
-      indexOfSpeed -= 1
-    }
-  }
+// function changeSpeed(type) {
+//   if (type == 'increase') {
+//     if (indexOfSpeed + 1 < speeds.length) {
+//       indexOfSpeed += 1
+//     }
+//   }
 
-  speed = speeds[indexOfSpeed]
-}
+//   if (type == 'decrease') {
+//     if (indexOfSpeed - 1 >= 0) {
+//       indexOfSpeed -= 1
+//     }
+//   }
+
+//   speed = speeds[indexOfSpeed]
+// }
 
 let n = 1;
 
@@ -28,28 +32,22 @@ let canvSection = document.getElementById('canvas');
 
 // Animation Start
 
-// Todo animating bug dare
-// back o front ------------------------
-
-function emptyAnimation() {
-  return {
+let animation = {
     state: 'stop',
     isStep: false,
     object: undefined,
     source: -1,
     target: -1,
     commands: [],
-  };
-}
+};
 
-let animation = emptyAnimation();
 
 // from to
 function move(f, t) {
   if (bars[f].disks.length <= 0) {
-    if (animation.state == 'animating') {
-    }
-    animation.state = 'done';
+    // if (animation.state == 'animating') {
+    //   animation.state = 'done';
+    // }
     return `Can't move`;
   }
 
@@ -62,11 +60,11 @@ function move(f, t) {
   };
   
   
-  let other = undefined;
-  if (bars[t].length <= 0) {
-    other = new Disk(0, 0)
+  let other = {};
+  if (bars[t].disks.length <= 0) {
     other.x = bars[t].x
-    other.y = 650 - bars[t].y - disk.height / 2
+    other.y = 650 - bars[t].y
+    other.height = 0
   } else {
     other = bars[t].disks.slice(-1)[0];
   }
@@ -81,15 +79,17 @@ function move(f, t) {
   animation.source = f;
   animation.target = t;
 
-  for (let i = 0; i < Math.abs(pointA.y - 30) / speed; i++) {
+  debugger
+
+  for (let i = 0; i < Math.abs(pointA.y - 30); i++) {
     animation.commands.push('up');
   }
 
-  for (let i = 0; i < Math.abs(pointA.x - pointB.x) / speed; i++) {
+  for (let i = 0; i < Math.abs(pointA.x - pointB.x); i++) {
     animation.commands.push(pointB.x - pointA.x > 0 ? 'right' : 'left');
   }
 
-  for (let i = 0; i < Math.abs(pointB.y - 30) / speed; i++) {
+  for (let i = 0; i < Math.abs(pointB.y - 30); i++) {
     animation.commands.push('down');
   }
 
@@ -103,10 +103,11 @@ function animate() {
       animation.object.render();
     } else {
       animation.state = 'done';
+      animation.object.render();
     }
   } else if (animation.state == 'done') {
     bars[animation.target].disks.push(animation.object);
-    animation = emptyAnimation();
+    animation.state = 'stop';
   }
 }
 // Animation End
@@ -145,6 +146,7 @@ function setup() {
 }
 
 function draw() {
+  frameRate(fr)
   background(200);
   sections.forEach((section) => section.render(mouseX, mouseY));
   bars.forEach((bar) => bar.render());
@@ -153,4 +155,5 @@ function draw() {
     let event = new CustomEvent('animation-end');
     document.dispatchEvent(event);
   }
+  debug_functions.forEach(fun => fun());
 }
