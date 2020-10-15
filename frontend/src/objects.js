900549b2ee3b162a7aed779b3fb664fac3a26957
Alireza <sleepingjempa@outlook.com>
2020-10-15 03:30:27 +0330

    animation method changed + app.js disabled ! please update this file

--- frontend/src/objects.js
+++ frontend/src/objects.js
@@ -6,9 +6,7 @@ class Disk {
     this.height = 20;
     this.n = radius;
     this.barIndex = barIndex;
-    this.R = random(255);
-    this.G = random(80, 220);
-    this.B = random(220, 250);
+    this.color = 255 * (1 - this.n % 2);
   }
 
   move(dir, speed) {
@@ -20,10 +18,10 @@ class Disk {
         this.x -= speed;
         break;
       case 'up':
-        this.y += speed;
+        this.y -= speed;
         break;
       case 'down':
-        this.y -= speed;
+        this.y += speed;
         break;
       default:
         break;
@@ -41,6 +39,7 @@ class Bar {
     this.disks = [];
     this.x = x;
     this.y = y;
+    this.freeY = 0;
     this.index = index;
   }
 
@@ -66,30 +65,40 @@ class Bar {
       disk.y = y;
       diff += disk.height;
     });
+
+    this.freeY = 650 - this.y -  diff;
+  }
+
+  pushDisk(disk) {
+    this.disks.push(disk);
+    this.update();
   }
 
   push(radius, type) {
     if (type == 'force') {
       let disk = new Disk(radius, this.index);
       this.disks.push(disk);
+      this.update();
       return true;
     } else {
       if (this.disks.length <= 13) {
         let disk = new Disk(radius, this.index);
         this.disks.push(disk);
+        this.update();
         return true;
       }
+      this.update();
       return true;
     }
   }
 
   pop() {
     return this.disks.pop();
+    this.update();
   }
 
   render() {
     line(this.x, this.y, this.x, 650 - this.y);
-    this.update();
     this.disks.forEach((disk) => disk.render());
   }
 }
@@ -127,5 +136,53 @@ class Section {
       this.y - this.height / 2 < y &&
       y < this.y + this.height / 2
     );
+  }
+}
+
+class DiskAnimation {
+  constructor(disk, action, condition, end, target) {
+    this.disk = disk;
+    this.action = action;
+    this.condition = condition;
+    this.end = end;
+    this.target = target
+  }
+
+  run() {
+    if (this.condition(this.disk)) {
+      this.action(this.disk);
+    } else {
+      this.end(this.disk, this.target);
+    }
+  }
+}
+
+class AnimationHandler {
+  constructor() {
+    this.state = 'free';
+    this.animations = [];
+  }
+
+  push(animation) {
+    this.state = 'busy';
+    this.animations.push(animation);
+  }
+
+  pop() {
+    return this.animations.pop();
+  }
+
+  run() {
+    if (this.animations.length === 0) {
+      if (this.state == 'busy') {
+        this.state = 'done';
+      } else {
+        this.state = 'free';
+      }
+    } else {
+      this.state = 'busy';
+      this.animations.slice(-1)[0].run();
+    }
+
   }
 }
--- frontend/src/view.js
+++ frontend/src/view.js
@@ -8,7 +8,7 @@ let debug_functions = [];
 
 // let indexOfSpeed = 2
 // let speeds = [1, 2, 5, 10]
-let speed = 1
+// let speed = 5
 
 // function changeSpeed(type) {
 //   if (type == 'increase') {
@@ -32,83 +32,138 @@ let canvSection = document.getElementById('canvas');
 
 // Animation Start
 
-let animation = {
-    state: 'stop',
-    isStep: false,
-    object: undefined,
-    source: -1,
-    target: -1,
-    commands: [],
-};
+// let animation = {
+//     state: 'stop',
+//     isStep: false,
+//     object: undefined,
+//     source: -1,
+//     target: -1,
+//     commands: [],
+// };
 
 
-// from to
-function move(f, t) {
-  if (bars[f].disks.length <= 0) {
-    // if (animation.state == 'animating') {
-    //   animation.state = 'done';
-    // }
-    return `Can't move`;
-  }
+// // from to
+// function move(f, t) {
+//   if (bars[f].disks.length <= 0) {
+//     // if (animation.state == 'animating') {
+//     //   animation.state = 'done';
+//     // }
+//     return `Can't move`;
+//   }
 
-  let disk = bars[f].pop();
+//   let disk = bars[f].pop();
 
   
-  let pointA = {
-    x: disk.x,
-    y: disk.y,
-  };
+//   let pointA = {
+//     x: disk.x,
+//     y: disk.y,
+//   };
   
   
-  let other = {};
-  if (bars[t].disks.length <= 0) {
-    other.x = bars[t].x
-    other.y = 650 - bars[t].y
-    other.height = 0
-  } else {
-    other = bars[t].disks.slice(-1)[0];
-  }
+//   let other = {};
+//   if (bars[t].disks.length <= 0) {
+//     other.x = bars[t].x
+//     other.y = 650 - bars[t].y
+//     other.height = 0
+//   } else {
+//     other = bars[t].disks.slice(-1)[0];
+//   }
   
-  let pointB = {
-    x: other.x,
-    y: other.y - other.height / 2 - disk.height / 2,
-  };
+//   let pointB = {
+//     x: other.x,
+//     y: other.y - other.height / 2 - disk.height / 2,
+//   };
 
-  animation.state = 'animating';
-  animation.object = disk;
-  animation.source = f;
-  animation.target = t;
+//   animation.state = 'animating';
+//   animation.object = disk;
+//   animation.source = f;
+//   animation.target = t;
 
-  debugger
+//   for (let i = 0; i < Math.abs(pointA.y - 30) / speed; i++) {
+//     animation.commands.push('up');
+//   }
 
-  for (let i = 0; i < Math.abs(pointA.y - 30); i++) {
-    animation.commands.push('up');
-  }
+//   for (let i = 0; i < Math.abs(pointA.x - pointB.x) / speed; i++) {
+//     animation.commands.push(pointB.x - pointA.x > 0 ? 'right' : 'left');
+//   }
 
-  for (let i = 0; i < Math.abs(pointA.x - pointB.x); i++) {
-    animation.commands.push(pointB.x - pointA.x > 0 ? 'right' : 'left');
-  }
+//   for (let i = 0; i < Math.abs(pointB.y - 30) / speed; i++) {
+//     animation.commands.push('down');
+//   }
 
-  for (let i = 0; i < Math.abs(pointB.y - 30); i++) {
-    animation.commands.push('down');
-  }
+//   // animation.commands.reverse()
+// }
 
-  // animation.commands.reverse()
-}
+// function animate() {
+//   if (animation.state == 'animating') {
+//     if (animation.commands.length > 0) {
+//       animation.object.move(animation.commands.pop(), speed);
+//       animation.object.render();
+//     } else {
+//       animation.state = 'done';
+//       animation.object.render();
+//     }
+//   } else if (animation.state == 'done') {
+//     bars[animation.target].disks.push(animation.object);
+//     animation.state = 'stop';
+//   }
+// }
 
-function animate() {
-  if (animation.state == 'animating') {
-    if (animation.commands.length > 0) {
-      animation.object.move(animation.commands.pop(), speed);
-      animation.object.render();
-    } else {
-      animation.state = 'done';
-      animation.object.render();
-    }
-  } else if (animation.state == 'done') {
-    bars[animation.target].disks.push(animation.object);
-    animation.state = 'stop';
-  }
+let animator = new AnimationHandler();
+let animationSpeed = 3;
+
+let move = (source, target) => {
+  if (bars[source].disks.length == 0) return "Can't move!!!!";
+
+  let disk = bars[source].pop();
+
+  let direction = (disk.x - bars[target].x < 0 ? 'right' : 'left');
+
+  animator.push(new DiskAnimation(
+    disk,
+    theDisk => {
+      theDisk.move('down', animationSpeed);
+      theDisk.render();
+    },
+    theDisk => {
+      return theDisk.y < bars[target].freeY - disk.height / 2;
+    },
+    (theDisk, target) => {
+      animator.pop();
+      bars[target].pushDisk(disk);
+    },
+    target
+  ));
+
+  animator.push(new DiskAnimation(
+    disk,
+    theDisk => {
+      theDisk.move(direction, animationSpeed);
+      theDisk.render();
+    },
+    theDisk => {
+      return Math.abs(theDisk.x - bars[target].x) > animationSpeed;
+    },
+    (theDisk, target) => {
+      animator.pop();
+    },
+    target
+  ));
+
+  animator.push(new DiskAnimation(
+    disk,
+    theDisk => {
+      theDisk.move('up', animationSpeed);
+      theDisk.render();
+    },
+    theDisk => {
+      return theDisk.y > 30;
+    },
+    (theDisk, target) => {
+      animator.pop();
+    },
+    target
+  ));
 }
 // Animation End
 
@@ -118,7 +173,7 @@ function nextDisk() {
 }
 
 function mousePressed() {
-  if (animation.state == 'animating') return
+  if (animator.state == 'busy') return
   sections.forEach((section) => {
     if (section.collision(mouseX, mouseY)) {
       if (bars[section.i].push(n, 'normal')) n++;
@@ -150,8 +205,8 @@ function draw() {
   background(200);
   sections.forEach((section) => section.render(mouseX, mouseY));
   bars.forEach((bar) => bar.render());
-  animate();
-  if (animation.state === 'done') {
+  animator.run()
+  if (animator.state === 'done') {
     let event = new CustomEvent('animation-end');
     document.dispatchEvent(event);
   }
